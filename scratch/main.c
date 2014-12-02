a04a87a3d76845bed97d9de966c54a91af985f8e
bculkin2442 <bjculkin@mix.wvu.edu>
2014-12-01 19:30:18 -0500

    Created a scratch workspace for random things not to put in their own project

--- scratch/main.c
+++ scratch/main.c
@@ -1,7 +1,9 @@
 //
 //  main.c
 //  scratch
+//  A workplace to put random code
 //
+//  An eponymous "hello, world"
 //  Created by Benjamin Culkin on 12/1/14.
 //  Copyright (c) 2014 bculkin. All rights reserved.
 //
