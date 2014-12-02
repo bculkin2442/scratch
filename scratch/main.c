91fd79fd5aeae6f1c2cfe6f102d9e7e14621a27b
bculkin2442 <bjculkin@mix.wvu.edu>
2014-12-01 19:29:10 -0500

    Initial Commit

---
+++ scratch/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  scratch
+//
+//  Created by Benjamin Culkin on 12/1/14.
+//  Copyright (c) 2014 bculkin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
